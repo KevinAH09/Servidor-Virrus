f7db30d5a9bfe9895def7bd0dd6beb22fa6743ec
KevinAH09 <55261485+KevinAH09@users.noreply.github.com>
2020-05-29 14:21:56 -0600

    cm

--- virussServidor/src/viruss/model/Servidor.java
+++ virussServidor/src/viruss/model/Servidor.java
@@ -11,6 +11,7 @@ import static java.lang.Boolean.FALSE;
 import java.util.List;
 import java.util.Timer;
 import java.util.TimerTask;
+import javafx.concurrent.Task;
 
 public class Servidor extends Conexion 
 {
@@ -29,6 +30,7 @@ public class Servidor extends Conexion
             if(tic>=60){
                 MainServidor.band = FALSE;
                 System.out.println("ServidorCerrado");
+                
             }
         }
     };
@@ -47,33 +49,34 @@ public class Servidor extends Conexion
                 
                 ObjectInputStream inObjeto = new ObjectInputStream( cs.getInputStream());
                 Juego lc =(Juego) inObjeto.readObject();          
-//                System.out.println(cs.getInetAddress().toString());
-//                String ip = cs.getInetAddress().toString();
-//                String replaceAll = ip.replaceAll("/", "");
+                System.out.println(cs.getInetAddress().toString());
+                String ip = cs.getInetAddress().toString();
+                String replaceAll = ip.replaceAll("/", "");
                 
-//                if(MainServidor.juegoMain.jugadores.isEmpty()){
-//                    MainServidor.juegoMain = lc;
-//                    MainServidor.juegoMain.jugadores.get(0).setIp(replaceAll);
-//                }else{
-//                    lc.jugadores.get(0).ip=replaceAll;
-//                    MainServidor.juegoMain.jugadores.add(lc.jugadores.get(0));                    
-//                }
-//                for(int i=0;i<lc.mazo.size();i++){
-//                    System.out.println(lc.mazo.get(i).color);
-//                }
+                if(MainServidor.juegoMain.jugadores.isEmpty()){
+                    timer.schedule(task, 10, 1000);
+                    MainServidor.juegoMain = lc;
+                    MainServidor.juegoMain.jugadores.get(0).setIp(replaceAll);
+                }else{
+                    lc.jugadores.get(0).ip=replaceAll;
+                    MainServidor.juegoMain.jugadores.add(lc.jugadores.get(0));                    
+                }
+                for(int i=0;i<lc.mazo.size();i++){
+                    System.out.println(lc.mazo.get(i).color);
+                }
                 
                 
-                // Empezamos dentro de 10ms y luego lanzamos la tarea cada 1000ms
-                timer.schedule(task, 10, 1000);
-//                Runnable nuevoCliente = new HiloCliente();
-//                Thread hilo = new Thread(nuevoCliente);
-//                hilo.start();
+//                 Empezamos dentro de 10ms y luego lanzamos la tarea cada 1000ms
+                
+                Runnable nuevoCliente = new HiloCliente();
+                Thread hilo = new Thread(nuevoCliente);
+                hilo.start();
                 System.out.println("Fin de la conexión");
                 ss.close();//Se finaliza la conexión con el cliente
                 cs.close();
                 inObjeto.close();
                
-                
+                        
                
                 
             
